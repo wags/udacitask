cdf466b9ad31efdd5863cf68463b3e250ba4dd8c
Matt Wagner <matt@wags.us>
2016-01-12 21:10:05 -0500

    Add sample code from the lesson
    Add the first few methods for this project, as outlined in the lesson.

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,25 @@
 class TodoList
-    # methods and stuff go here
+  attr_reader :title, :items
+
+  # Initialize todo list with a title and no items
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
+
+  # Create a new Item and add it to the array of Items
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
 end
 
 class Item
-    # methods and stuff go here
+  attr_reader :description, :completed_status
+
+  # Initialize item with a description and mark as not complete by default
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
+  end
 end
