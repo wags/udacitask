8778bf3d5902e325075265ba2bf791cdd9b156df
Matt Wagner <matt@wags.us>
2016-01-21 13:18:34 -0500

    Add code to satisfy basic requirements
    Add methods in todolist.rb and appropriate statements in udacitask.rb
    to satisfy the basic lesson requirements.

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,7 @@
 class TodoList
-  attr_reader :title, :items
+  attr_accessor :title, :items
 
-  # Initialize todo list with a title and no items
+  # Initialize todo list with a title and no Items
   def initialize(list_title)
     @title = list_title
     @items = Array.new
@@ -10,16 +10,63 @@ class TodoList
   # Create a new Item and add it to the array of Items
   def add_item(new_item)
     item = Item.new(new_item)
-    @items.push(item)
+    items.push(item)
+  end
+
+  def mark_item_complete(item_number)
+    items[item_number - 1].mark_complete
+  end
+
+  def rename(new_title)
+    @title = new_title
+  end
+
+  def delete_item_at_index(index)
+    items.delete_at(index)
+  end
+
+  def delete_item(item_number)
+    delete_item_at_index(item_number - 1)
+  end
+
+  def print_title
+    puts "-" * title.length
+    puts title
+    puts "-" * title.length
+  end
+
+  def print_items
+    items.each { |item| item.print_item }
+  end
+
+  def print_list
+    print_title
+    print_items
   end
 end
 
 class Item
-  attr_reader :description, :completed_status
+  attr_reader :description, :completed
 
-  # Initialize item with a description and mark as not complete by default
+  # Initialize Item with a description and mark as not complete by default
   def initialize(item_description)
     @description = item_description
-    @completed_status = false
+    @completed = false
+  end
+
+  def complete?
+    completed
+  end
+
+  def toggle_status
+    @completed = !@completed
+  end
+
+  def mark_complete
+    toggle_status if !complete?
+  end
+
+  def print_item
+    puts "#{description} - #{complete?}"
   end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,37 @@
-require './todolist.rb'
+require_relative "todolist"
 
 # Creates a new todo list
+list = TodoList.new("Dream List")
 
 # Add four new items
+list.add_item("Update resume")
+list.add_item("Get a new job")
+list.add_item("Meet with architect")
+list.add_item("Build new house")
 
 # Print the list
+list.print_list
 
 # Delete the first item
+list.delete_item 1
 
 # Print the list
+list.print_list
 
 # Delete the second item
+list.delete_item 2
 
 # Print the list
+list.print_list
 
 # Update the completion status of the first item to complete
+list.mark_item_complete 1
 
 # Print the list
+list.print_list
 
 # Update the title of the list
+list.rename("Current Goals")
 
 # Print the list
+list.print_list
